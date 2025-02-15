d9af3eaabb2769d742a64b9255f7c06d027c59b9
D_Ace <ode808prod@gmail.com>
2025-02-16 00:18:30 +0100

    nvim-full-setup-vs_code-alike-V1

---
+++ init.lua
@@ -0,0 +1,16 @@
+local username = "M!ster D. Ace"
+
+print("Welcome back " , username , "!\nHappy cracking!")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+
+require("vim-options")
+require("lazy").setup("plugins")
